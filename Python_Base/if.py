8c09a0a7ab230658343ac02e0a5828c64ba59cdd
minjunyue <minjunyue@trans-cosmos.com.cn>
2022-08-17 18:07:59 +0800

    if语句

---
+++ Python_Base/if.py
@@ -0,0 +1,28 @@
+# num = int(input('请输入一个任意的整数：'))
+# if num % 2 == 0:
+#     print("偶数")
+# else:
+#     print("奇数")
+
+# year = int(input('请输入一个任意的年份：'))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("闰年")
+# else:
+#     print("平年")
+
+# dog_age = float(input('请输入狗的年龄：'))
+# person_age = 0
+# if dog_age <= 2:
+#     person_age = dog_age * 10.5
+# else:
+#     person_age = 2 * 10.5 + (dog_age - 2) * 4
+#
+# print(person_age)
+
+# score = int(input('请输入小明的成绩（0-100）：'))
+# if score == 100:
+#     print("BMW")
+# elif score >= 80:
+#     print("iphone")
+# else:
+#     print("参考书")
